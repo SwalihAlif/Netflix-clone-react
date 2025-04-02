59355481715cd5f76792b85c9962c951c65dd329
Muhammed Swalih M <zzray485@gmail.com>
2025-04-02 16:48:45 +0530

    finised

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Home from './pages/Home/Home'
-import {Routes, Route} from 'react-router-dom'
+import {Routes, Route, useNavigate} from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Player from './pages/Player/Player'
+import { onAuthStateChanged } from 'firebase/auth'
+import { auth } from './firebase'
+import { ToastContainer, toast } from 'react-toastify';
+
+import { doc, getDoc } from "firebase/firestore";
+import { db } from "./firebase"; // Firestore instance
 
 const App = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        console.log("Logged In");
+
+        let name = user.displayName;
+        if (!name) {
+          const userDoc = await getDoc(doc(db, "users", user.uid));
+          name = userDoc.exists() ? userDoc.data().name : "Unknown User";
+        }
+        console.log("User Name: ", name)
+        
+
+        navigate('/');
+      } else {
+        console.log("Logged out");
+        navigate('/login'); 
+      }
+    })
+
+  }, [navigate])
+
   return (
     <div>
+      <ToastContainer theme='dark' />
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/login' element={<Login/>}/>
---
+++ src/firebase.js
@@ -0,0 +1,76 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+    createUserWithEmailAndPassword, 
+    getAuth, 
+    signInWithEmailAndPassword,
+    signOut,
+    updateProfile,
+    } from "firebase/auth";
+import {
+    addDoc, 
+    collection, 
+    getFirestore} from "firebase/firestore"
+import { toast } from "react-toastify";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBjMZnSq-8SqQAbtDsbKfxcj7_Syx98I8E",
+  authDomain: "netflix-clone-react-7d82e.firebaseapp.com",
+  projectId: "netflix-clone-react-7d82e",
+  storageBucket: "netflix-clone-react-7d82e.firebasestorage.app",
+  messagingSenderId: "555859897100",
+  appId: "1:555859897100:web:7221df582380a7703cf729"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+
+const signup = async (name, email, password) => {
+    try {
+        const res = await createUserWithEmailAndPassword(auth, email, password);
+        const user = res.user;
+
+        await updateProfile(user, {displayName: name})
+
+        await addDoc(collection(db, "users"), {
+            uid: user.uid,
+            name,
+            authProvider: "local",
+            email,
+        })
+    } catch (error) {
+
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+    }
+}
+
+const login = async (email, password) => {
+    try {
+        await signInWithEmailAndPassword(auth, email, password)
+    } catch (error) {
+        console.log(error);
+        toast.error(error.code.split('/')[1].split('-').join(" "));
+
+    }
+
+}
+const logout = async () => {
+
+    try {
+        await signOut(auth);
+        toast.info("Logged out successfully! 👋");
+
+    } catch (error) {
+        console.log(error);
+        toast.error("Failed to log out.");
+    }
+}
+
+export {auth, db, login, signup, logout};
